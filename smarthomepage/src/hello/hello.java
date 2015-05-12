63b3009d33a81b4f63601777e80be7257455e7c6
itbank <itbank@itbank-PC>
2015-05-12 15:22:06 +0900

    launching

---
+++ smarthomepage/src/hello/hello.java
@@ -0,0 +1,13 @@
+package hello ;
+
+	class hello {
+		public static void main(String[] args) {
+			int i ;
+			
+			for ( i = 0 ; i < 5 ; i++ ){
+			System.out.println("Hello Java!!");
+			}
+		}
+		
+	}
+
